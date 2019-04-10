2d6a2706b4fa0c3dc5a9a08fa57550a11dc39dc1
merl-mewiz <borodayal@gmail.com>
2019-04-10 11:21:57 +0400

    Базовые настройки, bootstrap + немного devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get 'users/profile'
+  get 'welcome/index'
+  root 'welcome#index'
+
+  get 'users/profile', as: 'user_root'
 end
